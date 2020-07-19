eef92a0d1a03204d2b2619d30203563e8a6ed98e
Scott Jellen <scottjellen@scotts-MBP.fios-router.home>
2020-07-19 15:37:23 -0400

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+
 function App() {
+  const [items, setItems] = useState([])
+  
+  
+  useEffect(() => {
+   fetch("https://programming-quotes-api.herokuapp.com/quotes/random")
+   .then(res => res.json())
+   .then(
+       (result) => {
+           setItems(result)
+           
+          
+       }
+   )
+   .catch(error => console.log(error))
+  }, [])
+
+
+  const reload = () => window.location.reload()
+   
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+              <blockquote className="quote-box" id="quote-box">
+                <p className="text" id="text">{items.en}</p>
+                <span className="author" id="author">-{items.author}</span>
+
+                <a href="twitter.com/intent/tweet" target="_blank" rel="noopener noreferrer" id="tweet-quote">Tweet</a>
+                <button  id="new-quote">Random</button>
+            </blockquote>
     </div>
   );
 }
