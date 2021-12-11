271122f7c9d75d6f4cf7380afab98299b955cc3b
SJellen <sjellen@me.com>
2021-12-11 11:51:52 -0500

    update and whitespace cleanup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
 
-
-
-
-
-
 function App() {
   const [items, setItems] = useState([])
   const [currentColor, setColor] = useState("#436f8a")
 
   const colorList = ["#221f3b","#394989","#0f4c75","#204051","#2f2519","#184d47","#562349","#1f4068","#30475e","#222831","#4d3e3e","#442727","#363636","#084177","#7d5e2a","#2d132c","#706c61","#272343"]
 
-
-  
   useEffect(() => {
    fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random")
    .then(res => res.json())
@@ -25,23 +18,18 @@ function App() {
    .catch(error => console.log(error))
   }, [])
 
-  console.log(items.quoteAuthor, items.quoteText)
-
   function handleClick() {
     let color = colorList[Math.floor(Math.random() * colorList.length)]
     setColor(color)
     fetch("https://quote-garden.herokuapp.com/api/v3/quotes/random")
    .then(res => res.json())
    .then(
-       (result) => {
-          
+       (result) => {        
            setItems(result.data[0])    
        }
    )
    .catch(error => console.log(error))
   }
-
-  
 
   let quote = items.quoteText
   let author = items.quoteAuthor
@@ -52,7 +40,6 @@ function App() {
       "text",
       `${quote} - ${author}`.slice(null, twitterMax)
     );
-   
 
   return (
     <div className="App  trans-color" style={{backgroundColor: currentColor}}>
